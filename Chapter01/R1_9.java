66428caa94025af19979c16ed688761d7a0f12e8
Eyad AL-‘Amawi <ealamawi@smail.ucas.edu.ps>
2017-05-06 11:05:21 +0300

    Add files via upload

---
+++ Chapter01/R1_9.java
@@ -0,0 +1,17 @@
+package Chapter01;
+
+// R1.9 What is the compile-time error in this program?
+public class R1_9 {
+
+    public static void main(String[] args) {
+
+        //System.out.println("Hello", "World!");
+    }
+}
+
+/*
+  Answer : 
+    compile-time error  (syntax error ) is comma between 
+    "Hello", "World!" .
+    to correct it replace comma with + . 
+ */
