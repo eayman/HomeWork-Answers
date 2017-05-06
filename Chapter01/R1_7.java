ebbd09d0b65757d2fc036a5005c1fbb804290468
Eyad AL-‘Amawi <ealamawi@smail.ucas.edu.ps>
2017-05-06 11:04:07 +0300

    Add files via upload

---
+++ Chapter01/R1_7.java
@@ -0,0 +1,20 @@
+package Chapter01;
+
+// R1.7 What does this program print?
+public class R1_7 {
+
+    public static void main(String[] args) {
+
+        System.out.println("39 + 3");
+        System.out.println(39 + 3);
+    }
+}
+
+/*
+  Answer : 
+The first statement System.out.println("39 + 3");
+print "39 + 3" in line and dawn line .
+
+Secend statement System.out.println(39 + 3);
+print the sum of two number 42. 
+ */
