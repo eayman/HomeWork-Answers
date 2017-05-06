5f3da02e8d8f9394e04c3ffb0e099117c45266cb
Eyad AL-‘Amawi <ealamawi@smail.ucas.edu.ps>
2017-05-06 10:39:16 +0300

    Create R1.9.java

---
+++ Chapter 1/R1.9.java
@@ -0,0 +1,17 @@
+
+// R1.9 What is the compile-time error in this program?
+
+public class Test {
+
+public static void main(String[] args) { 
+
+System.out.println("Hello", "World!");
+  }
+}
+
+/*
+  Answer : 
+    compile-time error  (syntax error ) is comma between 
+    "Hello", "World!" .
+    to correct it replace comma with + . 
+*/
